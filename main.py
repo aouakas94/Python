ec1728c69bcf35a7026d81bb84462a19c659d7e2
aouakas94 <cherifiaouakas8994@gmail.com>
2023-02-03 14:10:15 +0100

    ajout de fonction

--- main.py
+++ main.py
@@ -43,15 +43,61 @@ else:
     print (resultat)
 """
 #boucle while
-n=4
+"""
+n=8
 #print(n)
-n=n+1
+#n=n+1
 #print(n)
 
-while n < 10 :
-    
-    print(str(n))
-   
+while n <= 10
+    print(n)
+    n = n + 1
+"""
+"""
+#if
+#nom = input('nom?:')
+mot_de_passe = input('mot de passe?:')
 
-print ('fin de la boucle')
+while not mot_de_passe == "aouakas":
+        mot_de_passe = input('mot de passe erroné resaissez le:')
+print('azul aouakas')
+# video 27 debugeur a revoir
 
+
+#demander le nom pas vide
+nom = ""
+nom = input('nom?:')
+while nom == "":
+    nom = input('inser un nom?:')
+print("bonjour " + nom)
+"""
+
+#fonction
+
+
+
+
+
+def demande_age():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("quel est votre age? ")
+        try:
+            age_int = int(age_str)
+        except:
+            print("error entrez un nombre")
+    return age_int
+
+nom = ""
+while nom == "":
+    nom = input('inserez un nom?:')
+age = demande_age()
+
+print ("bonjour " + nom )
+print ("vous avez " + str(age) + "ans")
+
+
+
+
+
+ 
